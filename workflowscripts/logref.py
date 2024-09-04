1d378981c555ade80b0132a3ae97d8be8f9bbb64
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-09-04 12:09:29 -0500

    Update logref.py

--- workflowscripts/logref.py
+++ workflowscripts/logref.py
@@ -6,7 +6,7 @@ logrefdir = basedir + "logref.md";
 
 print(logrefdir);
 
-os.system('git add .', shell=True, stdout=subprocess.PIPE);
-# os.system('git commit -m "Updating logref.md" {0}'.format(), shell=True, stdout=subprocess.PIPE);
+os.system('git add .', shell=True);
+# os.system('git commit -m "Updating logref.md" {0}'.format(), shell=True);
 
 sys.stdout.flush()
