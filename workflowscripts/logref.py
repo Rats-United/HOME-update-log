e85bc58b90de9bbc2cf5784e6095f80cee63a6f2
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-09-04 16:33:26 -0500

    Update logref.py

--- workflowscripts/logref.py
+++ workflowscripts/logref.py
@@ -9,6 +9,7 @@ print(logrefdir);
 os.system('echo os is running');
 os.system('git add all');
 os.system('git commit -m "Updating logref.md" {0}'.format(logrefdir));
+os.system('git status');
 os.system('git push');
 
 sys.stdout.flush()
