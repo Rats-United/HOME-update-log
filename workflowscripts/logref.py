3197319e899745114bee93b75135f1736b529f6e
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-09-04 12:02:19 -0500

    Update logref.py

--- workflowscripts/logref.py
+++ workflowscripts/logref.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-dir_path = os.path.dirname(os.path.realpath(__file__));
+dir_path = os.path.dirname(os.path.realpath(__file__)).replace("workflowscripts", "");
 
 print(dir_path);
 
