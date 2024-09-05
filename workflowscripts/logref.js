5833eabc29bad1fcdf6258152aeb5f36ac5696f5
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-09-05 09:13:07 -0500

    Update logref.js

--- workflowscripts/logref.js
+++ workflowscripts/logref.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 const childproc = require("child_process");
 const spawn = childproc.spawn;
 const os = require('os');
