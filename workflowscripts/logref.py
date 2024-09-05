71d52177428f223996e2a83efe315ab54d9bd97f
shysolocup <88659700+shysolocup@users.noreply.github.com>
2024-09-05 08:21:07 -0500

    Update logref.py

--- workflowscripts/logref.py
+++ workflowscripts/logref.py
@@ -10,6 +10,8 @@ token = sys.argv[1];
 
 commands = [
     'echo os is running',
+    'git config user.name "github-actions[bot]"',
+    'git config user.email "41898282+github-actions[bot]@users.noreply.github.com"',  
     'git add {0}'.format(logrefdir),
     'git commit -m "Refreshed logref.md" {0}'.format(logrefdir),
     'git status',
@@ -17,7 +19,7 @@ commands = [
 ];
 
 if token and token != "undefined":
-    commands.insert(1, 'git remote add origin https://{0}@github.com/Rats-United/HOME-update-log.git'.format(token));
+    # commands.insert(1, 'git remote add origin https://{0}@github.com/Rats-United/HOME-update-log.git'.format(token));
 
 for command in commands:
     print(command);
